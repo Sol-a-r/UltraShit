2621f2acc067d62d2ddd9f2cbe291300a1ef461b
Non ya <33205264+Sol-a-r@users.noreply.github.com>
2018-02-08 15:27:40 -0600

    s

--- Ultrashit dev/UltraShit~Backup/assets/js/update-page-main.js
+++
@@ -1,3 +0,0 @@
-$("#link1").click(function () {
-    chrome.tabs.create({url: "http://www.technipages.com/google-chrome-prefetch"});
-});
--- Ultrashit dev/UltraShit~Backup/assets/modules/navbar.js
+++
@@ -1,8 +0,0 @@
-var navbar = angular.module('ultrasurfNavbar', []);
-
-navbar.directive('ultrasurfExtensionNavbar', function () {
-    return {
-        restrict: 'E',
-        templateUrl: 'templates/navbar.html'
-    };
-});
